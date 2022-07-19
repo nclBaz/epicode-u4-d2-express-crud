02d75a41ca98c44a9e93e9c94d2557caabddb557
Riccardo Gulin <riccardo@nucleode.com>
2022-07-19 12:00:39 +0200

    Adds POST & GET /users

--- src/apis/users/index.js
+++ src/apis/users/index.js
@@ -10,18 +10,64 @@
 
 */
 
-import express from "express"
+import express from "express" // 3RD PARTY MODULE (npm i express)
+import fs from "fs" // CORE MODULE (no need to install!)
+import { fileURLToPath } from "url" // CORE MODULE
+import { dirname, join } from "path" // CORE MODULE
+import uniqid from "uniqid"
 
 const usersRouter = express.Router() // a router is a set of similar endpoints grouped together
 
+// ***************************** HOW TO GET users.json PATH *******************************
+
+// target --> C:\Epicode\WebDev\2022\wd0222\U4-W1\epicode-u4-d2-express-crud\src\apis\users\users.json
+
+// 1. We gonna start from the current's file path --> C:\Epicode\WebDev\2022\wd0222\U4-W1\epicode-u4-d2-express-crud\src\apis\users\index.js
+// console.log("CURRENT FILE URL: ", import.meta.url)
+// console.log("CURRENT FILE PATH: ", fileURLToPath(import.meta.url))
+
+// 2. We can then obtain the parent's folder path --> C:\Epicode\WebDev\2022\wd0222\U4-W1\epicode-u4-d2-express-crud\src\apis\users\
+// console.log("PARENT FOLDER PATH: ", dirname(fileURLToPath(import.meta.url)))
+// 3. We can concatenate /users/ folder path with "users.json" --> C:\Epicode\WebDev\2022\wd0222\U4-W1\epicode-u4-d2-express-crud\src\apis\users\users.json
+const usersJSONPath = join(dirname(fileURLToPath(import.meta.url)), "users.json") // Please do NOT use "+" to concatenate 2 paths together. The RIGHT WAY is to use JOIN
+// console.log("TARGET: ", usersJSONPath)
+
+// ****************************************************************************************
+
 // 1. CREATE --> POST http://localhost:3001/users/ (+body)
 usersRouter.post("/", (req, res) => {
-  res.send({ message: "Hello I am the POST endpoint!" })
+  // 1. Read the request body to obtain new user's data
+  console.log("BODY: ", req.body) // remember to add express.json() into the main server configuration!!
+
+  // 2. Add some server generated informations (unique id, createdAt,...)
+  const newUser = { ...req.body, createdAt: new Date(), id: uniqid() }
+  console.log("NEW USER: ", newUser)
+
+  // 3. Read the content of users.json file (obtaining an array)
+  const usersArray = JSON.parse(fs.readFileSync(usersJSONPath))
+
+  // 4. Push new user to the array
+  usersArray.push(newUser)
+
+  // 5. Write the array back on the file
+
+  fs.writeFileSync(usersJSONPath, JSON.stringify(usersArray)) // We cannot pass an array here as a second argument
+
+  // 6. Send a proper response
+
+  res.status(201).send({ id: newUser.id })
 })
 
 // 2. READ --> GET http://localhost:3001/users/
 usersRouter.get("/", (req, res) => {
-  res.send({ message: "Hello I am the GET endpoint!" })
+  // 1. Read the content of users.json file
+  const fileContent = fs.readFileSync(usersJSONPath) // Here you obtain a BUFFER object, which is MACHINE READABLE ONLY
+  // 2. Obtain an array from that file
+  const usersArray = JSON.parse(fileContent)
+  // console.log("FILE CONTENT: ", usersArray) // JSON.parse() buffer --> array
+  // 3. Send back the array as a response
+
+  res.send(usersArray)
 })
 
 // 3. READ (single user) --> GET http://localhost:3001/users/:userId
